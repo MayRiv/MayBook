3b43e3a49de3926f71ece402721ff603e07e65ad
MayRiv <MayRiv@i.ua>
2014-08-03 16:02:23 +0300

    There one view for Search Controller.:

---
+++ MayBook/MayBook/Models/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MayBook.App_Data
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index()
+        {
+            
+            var context = new MayBookDataContext();
+            var users = context.Users;
+            return View(users);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username)
+        {
+            if (username.Length == 0) return RedirectToAction("Index");
+            var context = new MayBookDataContext();
+            var users = context.Users.Where(u => u.Name == username);
+            return View(users);
+        }
+    }
+}
