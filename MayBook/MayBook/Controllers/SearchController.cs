3288c885ff13d6ace879e0a9b6a23d68e45024cf
MayRiv <MayRiv@i.ua>
2014-08-03 15:44:16 +0300

    There are profile changing and search feature.

---
+++ MayBook/MayBook/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MayBook.App_Data
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string username)
+        {
+            var context = new MayBookDataContext();
+            var users = context.Users.Where(u => u.Name == username);
+            return View(users);
+        }
+    }
+}
